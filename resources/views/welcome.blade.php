af44a19a276f6892d792094c0d3d984d860dbf06
lora98 <loreanabgd@hotmail.com>
2021-09-04 19:54:44 +0200

    izmena2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,35 @@
 <x-layout>
+
 <x-slot name='title'>
     Home
+   
     </x-slot>
+<br>
+<html>
+    <head>
+        <style>
+            h1{
+                text-align:center;
+                padding:50px;
+                background-color:white;
+            }
+            
+
+           
+            </style>
+            </head>
+
+            <body>
+     
+<h1>Welcome to our store!</h1>
+        </body>
+        </html>
+
+
+
+
+
+
 
 
 </x-layout>
